aadbeb1a4bc3dd276a45e2c7187b92d5e9f87305
ben9955 <ben95ansari@gmail.com>
2024-05-14 16:15:15 +0100

    created the Navbar which will be showen if we have an user authenticated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,40 @@
-import React from "react";
+import { ThemeProvider, styled } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { lightTheme } from "./utils/Theme";
+import Authentications from "./pages/Authentications";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
 
 function App() {
-  return <div>App</div>;
+  const [user, setUser] = useState(true);
+
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        {user ? (
+          <Container>
+            <Navbar />
+          </Container>
+        ) : (
+          <Container>
+            <Authentications />
+          </Container>
+        )}
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
