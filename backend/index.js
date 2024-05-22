9125143aa0449268d65ecffe6ea6c74a4117cb47
ben9955 <ben95ansari@gmail.com>
2024-05-22 15:13:26 +0100

    connected to the mongo db and created a middleware toi handle errors

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
+import userRouter from "./routes/User.js";
 
 dotenv.config();
 
@@ -11,14 +12,37 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/user", userRouter);
+
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Helloo",
   });
 });
 
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    succes: false,
+    status,
+    message,
+  });
+});
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    const res = await mongoose.connect(process.env.DB_MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const startServer = async () => {
   try {
+    connectDB();
     app.listen(8080, () => {
       console.log("server running on port 8080");
     });
